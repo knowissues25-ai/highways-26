3dac53d3c809a6e4a268b9097f336d5c1d3a706b
Vis-halV <vishal06076@gmail.com>
2026-04-06 16:45:29 +0530

    feat: added vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useEffect, lazy, Suspense } from 'react'
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom'
+import { Analytics } from "@vercel/analytics/react"
 import './App.css'
 
 // Components
@@ -102,6 +103,7 @@ function App() {
 
         <PreFooter />
         <Footer />
+        <Analytics />
       </div>
     </Router>
   )
